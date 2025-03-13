ca2d0e5e5098e82c36e34811e899be646f3e7e7d
tiaprime <tiaprime@byu.edu>
2025-03-13 13:37:04 -0600

    base init

---
+++ backend/mission10/mission10/Models/ZtblSkipLabel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mission10.Models;
+
+public partial class ZtblSkipLabel
+{
+    public int LabelCount { get; set; }
+}
---
+++ backend/mission10/mission10/Models/ZtblWeek.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mission10.Models;
+
+public partial class ZtblWeek
+{
+    public DateOnly WeekStart { get; set; }
+
+    public DateOnly? WeekEnd { get; set; }
+}
